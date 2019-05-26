d6acae9952bbd6c2f1a0b7d70cdef0457a448889
kjs1005 <48340259+kjs1005@users.noreply.github.com>
2019-05-26 18:10:03 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(void) 
+{ 
+	FILE *fptr;
+	char word;
+	int count=0;
+	fptr=fopen("C:\\123.txt","r");
+	if(fptr!=NULL)
+	{
+		while((word=getc(fptr))!=EOF)
+		{
+			printf("%c",word);
+			count++;
+		}
+		fclose(fptr);
+	}
+	else
+		printf("ÀÉ®×¶}±Ò¥¢±Ñ!!\n");
+		printf("\n");
+system("pause"); 
+return 0; 
+} 
+
